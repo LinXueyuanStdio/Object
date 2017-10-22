abf46775e61b4151cd482de8dac4b7b64579cdd8
LinXueyuanStdio <761516186@qq.com>
2017-10-22 21:57:08 +0800

    add xylibrary

---
+++ xylibrary/src/main/java/pub/xylibrary/bone/order/Constant.java
@@ -0,0 +1,23 @@
+package pub.xylibrary.bone.order;
+
+/**
+ * @author dlinking-lxy
+ * @PackageName: pub.xylibrary.bone.order.
+ * @date 17-7-19
+ * @discription 公共资源类
+ */
+public class Constant {
+    //TODO 改为你的存图片的服务器地址
+    public static final String IMAGE_BASE_URL = "http://demo.upaiyun.com";
+    //TODO 改为你的正式服务器地址
+    public static final String URL_SERVER_ADDRESS_NORMAL_HTTP = "http://139.196.140.118:8080/";//正式服务器
+    //TODO 改为你的正式服务器地址
+    public static final String URL_SERVER_ADDRESS_NORMAL_HTTPS = "http://192.168.43.52:8080/";//正式服务器
+    //TODO 改为你的测试服务器地址,如果有的话
+    public static final String URL_SERVER_ADDRESS_TEST = "http://192.168.2.105:8080/";//测试服务器
+
+    //开发者信息设置
+    public static final String NICKNAME ="LinXueyuanStdio";
+    public static final String BLOG_URL ="https://LinXueyuanStdio.github.io/";
+    public static final String OTHER_URL ="https://github.com/LinXueyuanStdio";
+}
